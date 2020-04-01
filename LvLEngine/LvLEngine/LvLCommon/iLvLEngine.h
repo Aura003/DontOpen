02c0c88fc536a28bbf164dedfa4be17bcc306d81
aura03 <itsmemukil@gmail.com>
2020-04-01 11:39:44 +0800

    BaseDef and other changes on COmmon

--- LvLEngine/LvLEngine/LvLCommon/iLvLEngine.h
+++ LvLEngine/LvLEngine/LvLCommon/iLvLEngine.h
@@ -1,13 +1,31 @@
 #pragma once
+#include "LvLBaseDef.h"
+
+struct LvLWindowParams;
+
 class iLvLEngine 
 {
 
 public:
-	virtual void Initialize() = 0; //=0 makes it abstract 
+	virtual void Initialize(const LvLWindowParams& windowParams) =0; //=0 makes it abstract 
 	virtual void ShutDown() = 0;
 	virtual void StartEngine() = 0;
-	virtual void Update() = 0;
+	virtual bool Update() = 0;
 	virtual void Draw() = 0;
 private:
 protected:
+};
+
+struct LvLWindowParams 
+{
+public:
+	int PositionX = -1;
+	int PositionY = -1;
+	int Width = 640;
+	int Height = 480;
+
+	unsigned char Red;
+	unsigned char Green;
+	unsigned char Blue;
+	unsigned char Alpha;
 };
